6d74db4bdf22a02d806c8b004f4a7b500e3d8d2b
MariusCaear <kwakwamark@gmail.com>
2018-02-25 22:55:48 +0000

    Cleaned up app.comp.spec.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { DogListComponent } from './dog-list/dog-list.component';
 
+import { RouterModule } from '@angular/router';
+
 import { DogService } from './dog.service';
 import { DogListItemComponent } from './dog-list-item/dog-list-item.component';
 import { AppRoutingModule } from './/app-routing.module';
@@ -24,6 +26,7 @@ import { SearchPipePipe } from './search-pipe.pipe';
     AppRoutingModule,
     FormsModule
   ],
+  exports: [RouterModule],
   providers: [DogService],
   bootstrap: [AppComponent]
 })
